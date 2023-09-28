028672ffa8b707e61bb235e00cd451982af52579
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-09-28 19:35:05 +0100

    Candle done

--- tp1/MyCake.js
+++ tp1/MyCake.js
@@ -15,6 +15,9 @@ class MyCake  {
         this.z = z
         this.scene = scene
         this.mesh
+        this.candleMesh
+        this.fireMesh
+        this.fireMesh2
     }
 
     /**
@@ -25,12 +28,34 @@ class MyCake  {
             1, 1, 1,
             12, 2,
             false,
-            Math.PI * 0.25, Math.PI * 1.78 );
+            Math.PI * 0.25, Math.PI * 1.78);
 
-        let cakeMaterial = new THREE.MeshPhongMaterial({ color: "#FFC0CB"});
+        let cakeMaterial = new THREE.MeshPhongMaterial({ color: "#FFC0CB", side: THREE.DoubleSide});
         
         this.mesh = new THREE.Mesh(myCake, cakeMaterial);        
         this.mesh.position.set(this.x,this.y, this.z);
+
+        let myCandle = new THREE.CylinderGeometry(
+            0.1, 0.1, 0.8,
+            12);
+
+        let candleMaterial = new THREE.MeshPhongMaterial({ color: "#FFFFFF"});
+        
+        this.candleMesh = new THREE.Mesh(myCandle, candleMaterial);        
+        this.candleMesh.position.set(this.x,this.y + 0.9, this.z);
+
+        let halfSphere = new THREE.SphereGeometry( 0.1, 10, 5, 0, Math.PI * 2, 0, Math.PI / 2 );
+        let fireMaterial = new THREE.MeshPhongMaterial({ color: "#E25822", side: THREE.DoubleSide});
+        
+        this.fireMesh = new THREE.Mesh(halfSphere, fireMaterial);   
+        this.fireMesh.rotateX(Math.PI)
+        this.fireMesh.position.set(this.x,this.y + 1.4, this.z);
+
+        let cone = new THREE.ConeGeometry( 0.1, 0.4, 16 );
+        
+        this.fireMesh2 = new THREE.Mesh(cone, fireMaterial);   
+        //this.fireMesh.rotateX(Math.PI)
+        this.fireMesh2.position.set(this.x,this.y + 1.6, this.z);
     }
 }
 
--- tp1/MyTable.js
+++ tp1/MyTable.js
@@ -37,7 +37,6 @@ class MyTable  {
         let myLeg4= new MyLeg(-3,1.5,1.5,"leg", this.scene);
         let myLeg5= new MyLeg(0,5.55,0,"plate", this.scene);
         let myCake= new MyCake(0,5.56,0, this.scene);
-        let myCakeInner = new MyCake(0,5.56,0, this.scene);
         
         
         myLeg1.init();
@@ -46,7 +45,6 @@ class MyTable  {
         myLeg4.init();
         myLeg5.init();
         myCake.init();
-        myCakeInner.init()
 
         this.group.add(myLeg1.mesh);
         this.group.add(myLeg2.mesh);
@@ -54,7 +52,9 @@ class MyTable  {
         this.group.add(myLeg4.mesh);
         this.group.add(myLeg5.mesh);
         this.group.add(myCake.mesh);
-        this.group.add(myCakeInner.mesh);
+        this.group.add(myCake.candleMesh);
+        this.group.add(myCake.fireMesh)
+        this.group.add(myCake.fireMesh2)
 
     }
 }
