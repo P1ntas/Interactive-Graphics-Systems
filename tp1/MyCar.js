44c4e2c5a0663af6ca87828c94b6f7c0a9b194e8
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-10-13 22:28:08 +0100

    car fixed

--- tp1/MyCar.js
+++ tp1/MyCar.js
@@ -20,40 +20,50 @@ class MyCar  {
      */
     init() {
         // Define the control points for the Quadratic Bezier Curve
-        const startPoint = new THREE.Vector3(0, 0, 0);
-        const controlPoint = new THREE.Vector3(0.5, 1.1, 0); // Adjust the control point coordinates
-        const controlPoint_2 = new THREE.Vector3(1.5, 1.1, 0); // Adjust the control point coordinates
-        const endPoint = new THREE.Vector3(2, 0, 0);
+        const startPoint = new THREE.Vector3(0, 1, 0);
+        const controlPoint = new THREE.Vector3(0.55, 0.9987, 0); // Adjust the control point coordinates
+        const controlPoint_2 = new THREE.Vector3(0.9987, 0.55, 0); // Adjust the control point coordinates
+        const endPoint = new THREE.Vector3(1, 0, 0);
 
         const curve = new THREE.CubicBezierCurve3(startPoint, controlPoint, controlPoint_2, endPoint);
+        const curve1 = new THREE.CubicBezierCurve3(startPoint, controlPoint, controlPoint_2, endPoint);
 
         const curveGeometry = new THREE.BufferGeometry().setFromPoints(curve.getPoints(50)); // Adjust the number of points for smoothness
+        const curveGeometry1 = new THREE.BufferGeometry().setFromPoints(curve.getPoints(50)); // Adjust the number of points for smoothness
         const curveMaterial = new THREE.LineBasicMaterial({ color: 0xff0000 });
 
         this.curveObject = new THREE.Line(curveGeometry, curveMaterial);
+        this.curveObject1 = new THREE.Line(curveGeometry, curveMaterial);
         
         this.curveObject.rotateY(Math.PI / 2)
-        this.curveObject.position.set(-9.6,5.5,1)
+        this.curveObject1.rotateY(-Math.PI / 2)
+        this.curveObject.position.set(-9.6,5.5,-0)
+        this.curveObject1.position.set(-9.6,5.5,-0)
 
 
-        const startPoint2 = new THREE.Vector3(0, 0, 0);
-        const controlPoint2 = new THREE.Vector3(0.5, 1.1, 0); // Adjust the control point2 coordinates
-        const controlPoint2_1 = new THREE.Vector3(1.5, 1.1, 0); // Adjust the control point2 coordinates
-        const endPoint2 = new THREE.Vector3(2, 0, 0);
+        const startPoint2 = new THREE.Vector3(0, 1, 0);
+        const controlPoint2 = new THREE.Vector3(0.55, 0.9987, 0); // Adjust the control point2 coordinates
+        const controlPoint2_1 = new THREE.Vector3(0.9987, 0.55, 0); // Adjust the control point2 coordinates
+        const endPoint2 = new THREE.Vector3(1, 0, 0);
 
         const curve2 = new THREE.CubicBezierCurve3(startPoint2, controlPoint2, controlPoint2_1, endPoint2);
+        const curve2_1 = new THREE.CubicBezierCurve3(startPoint2, controlPoint2, controlPoint2_1, endPoint2);
 
         const curveGeometry2 = new THREE.BufferGeometry().setFromPoints(curve2.getPoints(50)); // Adjust the number of points for smoothness
+        const curveGeometry2_1 = new THREE.BufferGeometry().setFromPoints(curve2_1.getPoints(50)); // Adjust the number of points for smoothness
         const curveMaterial2 = new THREE.LineBasicMaterial({ color: 0xff0000 });
 
         this.curveObject2 = new THREE.Line(curveGeometry2, curveMaterial2);
+        this.curveObject2_1 = new THREE.Line(curveGeometry2_1, curveMaterial);
         
         this.curveObject2.rotateY(Math.PI / 2)
-        this.curveObject2.position.set(-9.6,5.5,-4)
+        this.curveObject2_1.rotateY(-Math.PI / 2)
+        this.curveObject2.position.set(-9.6,5.5,-5)
+        this.curveObject2_1.position.set(-9.6,5.5,-5)
 
-        const startPoint3 = new THREE.Vector3(0, 2, 0);
-        const controlPoint3_1 = new THREE.Vector3(1, 1.875, 0); // Adjust the control point2 coordinates
-        const controlPoint3_2 = new THREE.Vector3(1.5, 1.6, 0); // Adjust the control point2 coordinates
+        const startPoint3 = new THREE.Vector3(0.5, 1.4, 0);
+        const controlPoint3_1 = new THREE.Vector3(1.2, 1.4, 0); // Adjust the control point2 coordinates
+        const controlPoint3_2 = new THREE.Vector3(1.9, 1.2, 0); // Adjust the control point2 coordinates
         const endPoint3 = new THREE.Vector3(2, 0, 0);
 
         const curve3 = new THREE.CubicBezierCurve3(startPoint3, controlPoint3_1, controlPoint3_2, endPoint3);
@@ -66,10 +76,10 @@ class MyCar  {
         this.curveObject3.rotateY(Math.PI / 2)
         this.curveObject3.position.set(-9.6,5.5,-4)
 
-        const startPoint4 = new THREE.Vector3(0.3, 1.85, 0);
-        const controlPoint4_1 = new THREE.Vector3(1, 1.875, 0); // Adjust the control point2 coordinates
-        const controlPoint4_2 = new THREE.Vector3(1.5, 1.6, 0); // Adjust the control point2 coordinates
-        const endPoint4 = new THREE.Vector3(2.1, 0, 0);
+        const startPoint4 = new THREE.Vector3(0.3, 1.55, 0);
+        const controlPoint4_1 = new THREE.Vector3(1, 1.575, 0); // Adjust the control point2 coordinates
+        const controlPoint4_2 = new THREE.Vector3(2.3, 1.3, 0); // Adjust the control point2 coordinates
+        const endPoint4 = new THREE.Vector3(2.6, -0.6, 0);
 
         const curve4 = new THREE.CubicBezierCurve3(startPoint4, controlPoint4_1, controlPoint4_2, endPoint4);
 
@@ -82,9 +92,9 @@ class MyCar  {
         this.curveObject4.position.set(-9.6,7.5,-1.9)
 
 
-        const startPoint5 = new THREE.Vector3(1, 3.85, 0);
-        const controlPoint5_1 = new THREE.Vector3(2.5, 1.875*2, 0); // Adjust the control point2 coordinates
-        const controlPoint5_2 = new THREE.Vector3(1.5*2, 1.6*2, 0); // Adjust the control point2 coordinates
+        const startPoint5 = new THREE.Vector3(1, 3.55, 0);
+        const controlPoint5_1 = new THREE.Vector3(1.3, 1.775*2, 0); // Adjust the control point2 coordinates
+        const controlPoint5_2 = new THREE.Vector3(1.9*2, 1.5*2, 0); // Adjust the control point2 coordinates
         const endPoint5 = new THREE.Vector3(2.1*2, 0, 0);
 
         const curve5 = new THREE.CubicBezierCurve3(startPoint5, controlPoint5_1, controlPoint5_2, endPoint5);
@@ -99,7 +109,9 @@ class MyCar  {
 
 
         this.group.add(this.curveObject);
+        this.group.add(this.curveObject1);
         this.group.add(this.curveObject2);
+        this.group.add(this.curveObject2_1);
         this.group.add(this.curveObject3);
         this.group.add(this.curveObject4);
         this.group.add(this.curveObject5);
